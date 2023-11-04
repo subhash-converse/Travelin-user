906b256845b24581825135e3e8911e218346edd4
subhash-converse <133848850+subhash-converse@users.noreply.github.com>
2023-11-04 20:39:41 +0530

    fix

--- src/Components/LeftTourGridPage/GridCard.tsx
+++ src/Components/LeftTourGridPage/GridCard.tsx
@@ -3,74 +3,139 @@ import React, { useState } from "react";
 import carouselData from "../../Mock/PackageData";
 
 // ................Components.....................
-import Card from "../HomePage/Card";
+import LeftGridCard from "./LeftGridCard";
 import CardCarousel from "./CardCarousel";
-import { faAngleDown, faArrowRightLong, faBars, faTableCells } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faArrowRightLong,
+  faBars,
+  faTableCells,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import LeftLineCard from "./LeftLineCard";
 
 const GridCard = () => {
-  const [grid,setGrid]=useState(false)
+  const [grid, setGrid] = useState(false);
 
-  console.log(grid)
+  console.log(grid);
 
-  
+  const cardType = (value:boolean,icon:string) =>{
+      setGrid(value);
+      let A = document.getElementById(icon)
+      if(A){A.style.backgroundColor = "#ffbc00"}
+      
+  }
+
   return (
     <>
       <div className="w-full flex justify-center text-[16px] text-[#777777] font-thin ">
         <div className="w-full flex flex-col lg:max-w-[1290px] lg:flex-row  mt-[52px]">
           <div className="w-full lg:w-[66.66%] flex flex-col  px-4  items-center">
             <div className="mb-[35px] md:flex md:justify-between md:w-full">
-                <div className="flex items-center">
-                   <h1 className="">Showing 1-5 of 80 results</h1>
-                </div>
+              <div className="flex items-center">
+                <h1 className="">Showing 1-5 of 80 results</h1>
+              </div>
               <div className="flex gap-5 items-center">
-                <FontAwesomeIcon icon={faBars} onClick={()=>{setGrid(true)}} />
-                <FontAwesomeIcon icon={faTableCells} onClick={()=>{setGrid(false)}}/>
+                <FontAwesomeIcon
+                  icon={faBars}
+                  className="p-2 rounded-md"
+                  id="bar"
+                  onClick={() => {
+                    cardType(true,"bar");
+                  }}
+                />
+                <FontAwesomeIcon
+                  icon={faTableCells}
+                  id="cell"
+                  className="p-2 rounded-md"
+                  onClick={() => {
+                    cardType(false,"cell");
+                  }}
+                />
                 <div className="dropdown inline-block relative ">
                   <button className="nav w-[150px] font-semibold py-2 px-4 border rounded-lg inline-flex justify-between items-center">
-                    <span>Sort By{" "}</span>
-                    <span><FontAwesomeIcon icon={faAngleDown} /></span>
+                    <span>Sort By </span>
+                    <span>
+                      <FontAwesomeIcon icon={faAngleDown} />
+                    </span>
                   </button>
 
                   <ul className="dropdown-content absolute hidden nav w-[180px]  pt-1 z-10">
-                    <li><a className="rounded-t bg-white hover:bg-[#868686] hover:text-white py-2 px-4 block whitespace-no-wrap" href="#">
-                           Sort By
-                        </a>
+                    <li>
+                      <a
+                        className="rounded-t bg-white hover:bg-[#868686] hover:text-white py-2 px-4 block whitespace-no-wrap"
+                        href="#"
+                      >
+                        Sort By
+                      </a>
                     </li>
-                    <li><a className="bg-white hover:bg-[#868686] hover:text-white py-2 px-4 block whitespace-no-wrap" href="#">
-                            Average rating
-                        </a>
+                    <li>
+                      <a
+                        className="bg-white hover:bg-[#868686] hover:text-white py-2 px-4 block whitespace-no-wrap"
+                        href="#"
+                      >
+                        Average rating
+                      </a>
                     </li>
-                    <li><a className="bg-white hover:bg-[#868686] hover:text-white py-2 px-4 block whitespace-no-wrap" href="#">
-                            Price: low to high
-                        </a>
+                    <li>
+                      <a
+                        className="bg-white hover:bg-[#868686] hover:text-white py-2 px-4 block whitespace-no-wrap"
+                        href="#"
+                      >
+                        Price: low to high
+                      </a>
                     </li>
-                    <li><a className="bg-white hover:bg-[#868686] hover:text-white py-2 px-4 block whitespace-no-wrap" href="#">
-                            Price: high to low
-                        </a>
+                    <li>
+                      <a
+                        className="bg-white hover:bg-[#868686] hover:text-white py-2 px-4 block whitespace-no-wrap"
+                        href="#"
+                      >
+                        Price: high to low
+                      </a>
                     </li>
                   </ul>
                 </div>
               </div>
-            
-             
             </div>
 
-            <div className=" grid grid-cols-1 md:grid-cols-2 gap-7 py-5">
-              {carouselData.map((data) => {
-                let filter = data.catogory.find(
-                  (a) => a.toLowerCase().trim() === "left grid"
-                );
-                if (filter) {
-                  return <Card data={data} />;
-                } else {
-                  return null;
-                }
-              })}
-            </div>
+            {grid ? (
+              <div className="grid grid-cols-1 gap-7 py-5">
+                  {carouselData.map((data) => {
+                  let filter = data.catogory.find(
+                    (a) => a.toLowerCase().trim() === "left grid"
+                  );
+                  if (filter) {
+                    return <LeftLineCard data={data} gridtype={grid} />;
+                  } else {
+                    return null;
+                  }
+                })}
+              </div>
+            ) : (
+              <div className=" grid grid-cols-1 md:grid-cols-2 gap-7 py-5">
+                {carouselData.map((data) => {
+                  let filter = data.catogory.find(
+                    (a) => a.toLowerCase().trim() === "left grid"
+                  );
+                  if (filter) {
+                    return <LeftGridCard data={data} gridtype={grid} />;
+                  } else {
+                    return null;
+                  }
+                })}
+              </div>
+            )}
+
             <div className="flex justify-center">
               <div>
-                <button className="button tour-pic-btn py-[8px] flex flex-row gap-2"><div className="text">Load more <span><FontAwesomeIcon icon={faArrowRightLong}/></span></div></button>
+                <button className="button tour-pic-btn py-[8px] flex flex-row gap-2">
+                  <div className="text">
+                    Load more{" "}
+                    <span>
+                      <FontAwesomeIcon icon={faArrowRightLong} />
+                    </span>
+                  </div>
+                </button>
               </div>
             </div>
           </div>
@@ -78,129 +143,162 @@ const GridCard = () => {
           <div className=" w-full lg:w-[33.33%]">
             <div className="pl-[24px] pr-[15px] sticky top-0">
               <div className=" border-b-[1px] border-dashed border-[#777777] pb-[30px] mb-[32px]">
-                <h1 className="text-[26px] Play-fair text-[#17233E] border-b-[1px] mb-[30px] pb-[10px] ">Categories Type</h1>
+                <h1 className="text-[26px] Play-fair text-[#17233E] border-b-[1px] mb-[30px] pb-[10px] ">
+                  Categories Type
+                </h1>
                 <div className="grid grid-cols-1 gap-3">
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="Tours" type="checkbox" />
-                    <label htmlFor="Tours">Tours</label>
+                      <input id="Tours" type="checkbox" />
+                      <label htmlFor="Tours">Tours</label>
                     </div>
-                    <div><h2>92</h2></div>
+                    <div>
+                      <h2>92</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="Attractions" type="checkbox" />
-                    <label htmlFor="Attractions">Attractions</label>
+                      <input id="Attractions" type="checkbox" />
+                      <label htmlFor="Attractions">Attractions</label>
                     </div>
-                    <div><h2>22</h2></div>
+                    <div>
+                      <h2>22</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="DayTrips" type="checkbox" />
-                    <label htmlFor="DayTrips"> Day Trips</label>
+                      <input id="DayTrips" type="checkbox" />
+                      <label htmlFor="DayTrips"> Day Trips</label>
                     </div>
-                    <div><h2>35</h2></div>
+                    <div>
+                      <h2>35</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="OutdoorActivities" type="checkbox" />
-                    <label htmlFor="OutdoorActivities">Tours</label>
+                      <input id="OutdoorActivities" type="checkbox" />
+                      <label htmlFor="OutdoorActivities">Tours</label>
                     </div>
-                    <div><h2>41</h2></div>
+                    <div>
+                      <h2>41</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="Concert&Show" type="checkbox" />
-                    <label htmlFor="Concert&Show">Concert & Show</label>
+                      <input id="Concert&Show" type="checkbox" />
+                      <label htmlFor="Concert&Show">Concert & Show</label>
                     </div>
-                    <div><h2>61</h2></div>
+                    <div>
+                      <h2>61</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="Indoor" type="checkbox" />
-                    <label htmlFor="Indoor">Indoor</label>
+                      <input id="Indoor" type="checkbox" />
+                      <label htmlFor="Indoor">Indoor</label>
                     </div>
-                    <div><h2>18</h2></div>
+                    <div>
+                      <h2>18</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="SightSeeing" type="checkbox" />
-                    <label htmlFor="SightSeeing">Sight Seeing</label>
+                      <input id="SightSeeing" type="checkbox" />
+                      <label htmlFor="SightSeeing">Sight Seeing</label>
                     </div>
-                    <div><h2>88</h2></div>
+                    <div>
+                      <h2>88</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="Travels" type="checkbox" />
-                    <label htmlFor="Travels">Travels</label>
+                      <input id="Travels" type="checkbox" />
+                      <label htmlFor="Travels">Travels</label>
                     </div>
-                    <div><h2>92</h2></div>
+                    <div>
+                      <h2>92</h2>
+                    </div>
                   </div>
                 </div>
               </div>
 
               <div className=" border-b-[1px] border-dashed border-[#777777] pb-[30px] mb-[32px]">
-                <h1 className="text-[26px] Play-fair text-[#17233E] border-b-[1px] mb-[30px] pb-[10px] ">Duration Type</h1>
+                <h1 className="text-[26px] Play-fair text-[#17233E] border-b-[1px] mb-[30px] pb-[10px] ">
+                  Duration Type
+                </h1>
                 <div className="grid grid-cols-1 gap-3">
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="Tours" type="checkbox" />
-                    <label htmlFor="Tours">up to 1 hour</label>
+                      <input id="Tours" type="checkbox" />
+                      <label htmlFor="Tours">up to 1 hour</label>
                     </div>
-                    <div><h2>92</h2></div>
+                    <div>
+                      <h2>92</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="Attractions" type="checkbox" />
-                    <label htmlFor="Attractions">1 to 2 hour</label>
+                      <input id="Attractions" type="checkbox" />
+                      <label htmlFor="Attractions">1 to 2 hour</label>
                     </div>
-                    <div><h2>22</h2></div>
+                    <div>
+                      <h2>22</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="DayTrips" type="checkbox" />
-                    <label htmlFor="DayTrips">  2 to 4 hour</label>
+                      <input id="DayTrips" type="checkbox" />
+                      <label htmlFor="DayTrips"> 2 to 4 hour</label>
                     </div>
-                    <div><h2>35</h2></div>
+                    <div>
+                      <h2>35</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="OutdoorActivities" type="checkbox" />
-                    <label htmlFor="OutdoorActivities">4 to 8 hour</label>
+                      <input id="OutdoorActivities" type="checkbox" />
+                      <label htmlFor="OutdoorActivities">4 to 8 hour</label>
                     </div>
-                    <div><h2>41</h2></div>
+                    <div>
+                      <h2>41</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
                     <div className="input-lable">
-                    <input id="Concert&Show" type="checkbox" />
-                    <label htmlFor="Concert&Show"> 8 to 1 Day</label>
+                      <input id="Concert&Show" type="checkbox" />
+                      <label htmlFor="Concert&Show"> 8 to 1 Day</label>
                     </div>
-                    <div><h2>61</h2></div>
+                    <div>
+                      <h2>61</h2>
+                    </div>
                   </div>
                   <div className="checkbox-inputs">
-                    <div className="input-lable"> 
-                    <input id="Indoor" type="checkbox" />
-                    <label htmlFor="Indoor"> 1 Day to 2 Days</label>
+                    <div className="input-lable">
+                      <input id="Indoor" type="checkbox" />
+                      <label htmlFor="Indoor"> 1 Day to 2 Days</label>
                     </div>
-                    <div><h2>18</h2></div>
+                    <div>
+                      <h2>18</h2>
+                    </div>
                   </div>
                 </div>
               </div>
-                
-              <div className=" border-b-[1px] border-dashed border-[#777777] pb-[30px] mb-[32px]">
-                <h1 className="text-[26px] Play-fair text-[#17233E] border-b-[1px] mb-[30px] pb-[10px] ">Duration Type</h1>
-                  <h2 className="mb-[16px]">Price Range</h2>
-                  <input type="range" className="w-full" />
-                
 
+              <div className=" border-b-[1px] border-dashed border-[#777777] pb-[30px] mb-[32px]">
+                <h1 className="text-[26px] Play-fair text-[#17233E] border-b-[1px] mb-[30px] pb-[10px] ">
+                  Duration Type
+                </h1>
+                <h2 className="mb-[16px]">Price Range</h2>
+                <input type="range" className="w-full" />
               </div>
 
               <div className="  pb-[30px] mb-[32px]">
-                <h1 className="text-[26px] Play-fair text-[#17233E] border-b-[1px] mb-[30px] pb-[10px] ">Related Destinations</h1>
-                 <div className=" ">
-                 <CardCarousel carouselData={carouselData} />
-   
-                 </div>
+                <h1 className="text-[26px] Play-fair text-[#17233E] border-b-[1px] mb-[30px] pb-[10px] ">
+                  Related Destinations
+                </h1>
+                <div className=" ">
+                  <CardCarousel carouselData={carouselData} />
+                </div>
               </div>
             </div>
           </div>
---
+++ src/Components/LeftTourGridPage/LeftLineCard.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+
+import topPick2 from "../../Assets/Images/HomeImage/TopPick2.jpg"
+
+// <========== icons ==========>
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+
+interface data {
+    data: {
+      image: any;
+      country: string;
+      touristPlace: string;
+      reviewsStar: number;
+      description: string;
+      catogory: string[];
+      price: number;
+      territory: string;
+      days: number;
+      guideName?: string;
+      position?: string;
+    },
+    gridtype:boolean
+  }
+
+const LeftLineCard = (props:data) => {
+  return (
+    <div className=' grid grid-cols-1 md:grid-cols-3 gap-4 last-deal card-hover card-line  bg-white main-input p-7'>
+        <div>
+            <img src={topPick2} alt="tourpic" className='rounded-xl w-full h-full' />
+        </div>
+        <div>
+            <p className='text-[#777777] text-[13px]'>6+ Hours | Full Day Tours </p>
+            <h2 className='Play-fair text-[26px] text-[#17233E] pb-[8px]'>Leeds Castle, Cliffs Of Dover</h2>
+            <div>
+                <div>
+                    <LocationOnOutlinedIcon
+                    style={{ height: "10", width: "10px" }}
+                    className="h-[39px]  justify-around"/>
+                </div>
+                <div className='Play-fair text-[#029E9D]'>{props.data.country}</div>
+            </div>
+            <p className='mt-[32px] text-[#777777]'>Taking Safety Measures</p>
+            <p className='text-[#029E9D]'>Free cancellation</p>
+        </div>
+        <div className='justify-self-end text-right'>
+             <div className="rating-main pb-1 flex justify-end">
+               <div className="rating text-[#FDC703]  ">
+                    <span className="fa fa-star checked"><FontAwesomeIcon icon={faStar} /></span>
+                    <span className="fa fa-star checked"><FontAwesomeIcon icon={faStar} /></span>
+                    <span className="fa fa-star checked"><FontAwesomeIcon icon={faStar} /></span>
+                    <span className="fa fa-star checked"><FontAwesomeIcon icon={faStar} /></span>
+                    <span className="fa fa-star checked"><FontAwesomeIcon icon={faStar} /></span>
+
+              </div>
+            </div>
+            <p className='text-[13px] text-[#777777]'>200 Reviews</p>
+            <div className='flex flex-col my-[16px]'>
+                <span>From</span>
+                <span>{`$${props.data.price}`}</span>
+                <span>Per Adult</span>
+            </div>
+            <button className="button tour-pic-btn py-[8px] flex flex-row gap-2">
+                  <div className="text">
+                    View Detail{" "}
+                  </div>
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default LeftLineCard
